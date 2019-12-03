80b0bd63270a7338b291fe4c21bed0b0b69ac59d
billa <billamullai8@gmail.com>
2019-12-03 10:23:10 +0530

    change sample

--- sample/src/sample/Sample.java
+++ sample/src/sample/Sample.java
@@ -15,6 +15,7 @@ public class Sample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hello billa");
         // TODO code application logic here
     }
     
